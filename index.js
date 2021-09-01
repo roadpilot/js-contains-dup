96161f8d0008d259e475152b4f4bc48e27645571
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-01 14:45:04 +0000

    refactored

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums = nums.sort()
-    const state = {}
-    for (i of nums){
-        if (state[i]){return true}
-        else {state[i]=1}
-    }
-    return false
+    // nums = nums.sort()
+    // const state = {}
+    // for (i of nums){
+    //     if (state[i]){return true}
+    //     else {state[i]=1}
+    // }
+    // return false
+    return nums.length !== new Set(nums).size
 };
