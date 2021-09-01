8a926236800b7ee8c169377bc4e4b2d83713e9b3
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-01 13:40:11 +0000

    solution

--- index.js
+++ index.js
@@ -3,5 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  
+    nums = nums.sort()
+    const state = {}
+    for (i of nums){
+        if (state[i]){return true}
+        else {state[i]=1}
+    }
+    return false
 };
